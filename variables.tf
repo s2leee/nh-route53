c107ea57a5944901c12840f2328dba073feefe48
s2leee <89497631+s2leee@users.noreply.github.com>
2023-03-27 13:18:36 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "record_name" {
+     type = list(string)
+}
+
