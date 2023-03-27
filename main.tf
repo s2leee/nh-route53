8e41e61cf5d5a09454c046067bbefa5f7aa69b59
s2leee <89497631+s2leee@users.noreply.github.com>
2023-03-27 14:35:36 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,12 @@ resource "aws_route53_record" "route53_record" {
         zone_id = aws_route53_zone.route53_zone.zone_id
         name = var.record_name
         type = "A"
-
+/*
         alias {
                 name = data.aws_lb.elb_data.dns_name
                 zone_id = data.aws_lb.elb_data.zone_id
                 evaluate_target_health = true
         }
+*/
+                
 }
