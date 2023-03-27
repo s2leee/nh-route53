552d8963e10e3923a93ea43ccb1145eaf06c46aa
s2leee <89497631+s2leee@users.noreply.github.com>
2023-03-27 16:04:54 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.33.0"
-    }
-  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
  
 
 provider "aws"{
-access_key  = "${var.access_key}"
-secret_key  = "${var.secret_key}"
+access_key  = var.access_key
+secret_key  = var.secret_key
 region = "ap-northeast-2"
 }
 
