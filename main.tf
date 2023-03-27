5d3cd2871008e3ba91263b0efb66cd4927dca33b
s2leee <89497631+s2leee@users.noreply.github.com>
2023-03-27 15:52:42 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,17 +3,17 @@ resource "aws_lb" "nh_alb" {
   name               = "nh-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = var.is_portal_sg == true ? [data.aws_security_group.kibo-sg[0].id] : [aws_security_group.sg[0].id]
-  #subnets            = [var.is_portal_subnet == true ? data.aws_subnet.kibo-subnet-01[0].id : aws_subnet.new_subnet[0].id]
+  security_groups    = var.is_portal_sg == true ? [data.aws_security_group.selee-secu[0].id] : [aws_security_group.sg[0].id]
+  #subnets            = [var.is_portal_subnet == true ? data.aws_subnet.selee-subnet[0].id : aws_subnet.new_subnet[0].id]
   #subnets            =  [for subnet in aws_subnet.new_subnet : subnet.id]
   dns_name              = "test_dns"
         
    subnet_mapping {
-    subnet_id            = var.is_portal_subnet == true ? data.aws_subnet.kibo-subnet-01[0].id : aws_subnet.new_subnet[0].id
+    subnet_id            = var.is_portal_subnet == true ? data.aws_subnet.selee-subnet[0].id : aws_subnet.new_subnet[0].id
     #private_ipv4_address = "10.0.1.15"
   }
  subnet_mapping {
-    subnet_id            = var.is_portal_subnet == true ? data.aws_subnet.kibo-subnet-01[0].id : aws_subnet.lb_subnet[0].id
+    subnet_id            = var.is_portal_subnet == true ? data.aws_subnet.selee-subnet[0].id : aws_subnet.lb_subnet[0].id
     #private_ipv4_address = "10.0.2.15"
   }
   enable_deletion_protection = false
