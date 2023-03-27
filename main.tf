5600475c259247203696045f3b4c2b782ac7797d
s2leee <89497631+s2leee@users.noreply.github.com>
2023-03-27 15:22:53 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,28 @@
+# ALB
+resource "aws_lb" "nh_alb" {
+  name               = "nh-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = var.is_portal_sg == true ? [data.aws_security_group.kibo-sg[0].id] : [aws_security_group.sg[0].id]
+  #subnets            = [var.is_portal_subnet == true ? data.aws_subnet.kibo-subnet-01[0].id : aws_subnet.new_subnet[0].id]
+  #subnets            =  [for subnet in aws_subnet.new_subnet : subnet.id]
+  dns_name              = "test_dns"
+        
+   subnet_mapping {
+    subnet_id            = var.is_portal_subnet == true ? data.aws_subnet.kibo-subnet-01[0].id : aws_subnet.new_subnet[0].id
+    #private_ipv4_address = "10.0.1.15"
+  }
+ subnet_mapping {
+    subnet_id            = var.is_portal_subnet == true ? data.aws_subnet.kibo-subnet-01[0].id : aws_subnet.lb_subnet[0].id
+    #private_ipv4_address = "10.0.2.15"
+  }
+  enable_deletion_protection = false
+  
+  tags = {
+    Environment = "production"
+  }
+}
+
 resource "aws_route53_zone" "route53_zone" {
         name = var.name
 }
@@ -7,12 +32,12 @@ resource "aws_route53_record" "route53_record" {
         zone_id = aws_route53_zone.route53_zone.zone_id
         name = each.value.host
         type = "A"
-/*
+
         alias {
-                name = data.aws_lb.elb_data.dns_name
-                zone_id = data.aws_lb.elb_data.zone_id
+                name = aws_lb.nh_alb.dns_name
+                zone_id = aws_route53_zone.route53_zone.zone_id
                 evaluate_target_health = true
         }
-*/
+
                 
 }
