f879afa9ea22fa5dda2bc0d9f73aed259519bceb
s2leee <89497631+s2leee@users.noreply.github.com>
2023-03-27 15:56:54 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_lb" "nh_alb" {
   security_groups    = ["sg-05f6833f701143e65"]
   #subnets            = [var.is_portal_subnet == true ? data.aws_subnet.selee-subnet[0].id : aws_subnet.new_subnet[0].id]
   #subnets            =  [for subnet in aws_subnet.new_subnet : subnet.id]
-  dns_name              = "test_dns"
+  #dns_name              = "test_dns"
         
    subnet_mapping {
     subnet_id            = "subnet-0d68b1d3f2e7e1ebb"
