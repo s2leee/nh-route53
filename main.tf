7f49336135123c4b60d735923169dc36f33e7cdf
s2leee <89497631+s2leee@users.noreply.github.com>
2023-03-27 13:17:49 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_route53_zone" "route53_zone" {
+        name = var.name
+}
+
+resource "aws_route53_record" "route53_record" {
+        for_each = var.recod_name
+        zone_id = aws_route53_zone.route53_zone.zone_id
+        name = var.record_name
+        type = "A"
+
+        alias {
+                name = data.aws_lb.elb_data.dns_name
+                zone_id = data.aws_lb.elb_data.zone_id
+                evaluate_target_health = true
+        }
+}
